498dd69444555665e13e3ae677c1f2ce9e07661a
Pedro <pedrogrego1@outlook.com>
2022-11-26 20:01:14 -0300

    Atualizando estudos e desafios js

--- desafio-100-days/exercicios-rockseat/javascript/desafio1.js
+++ desafio-100-days/exercicios-rockseat/javascript/desafio1.js
@@ -9,22 +9,92 @@ Crie um algoritmo que transforme as notas do sistema numérico para sistema de n
 */
 
 //MINHA RESOLUÇÃO
-let nota = prompt("Nota: ")
-if(nota < 60){
-    nota = 'F'
-    console.log(nota)
-} else if(nota <= 70){
-    nota = 'D'
-    console.log(nota)
-} else if(nota <= 80){
-    nota = 'C'
-    console.log(nota)
-} else if(nota <= 90){
-    nota = 'B'
-    console.log(nota)
-} else{
-    nota = 'A'
-    console.log(nota)
-}
+// let nota = prompt("Nota: ") - Opção para digitar na tela
+let nota = 100
+    if(nota < 60){
+        nota = 'F'
+        console.log(nota)
+    } else if(nota <= 70){
+        nota = 'D'
+        console.log(nota)
+    } else if(nota <= 80){
+        nota = 'C'
+        console.log(nota)
+    } else if(nota <= 90){
+        nota = 'B'
+        console.log(nota)
+    } else{
+        nota = 'A'
+        console.log(nota)
+    }
 
 //RESOLUÇÃO ROCKSEAT
+let score = 100
+let scoreA = score >= 90 && score <= 100
+let scoreB = score >= 80 && score <= 89
+let scoreC = score >= 70 && score <= 79
+let scoreD = score >= 60 && score <= 69
+let scoreF = score < 60 && score >= 0
+let scoreFinal;
+
+    if (scoreA){
+        scoreFinal = 'A'
+    } else if(scoreB){
+        scoreFinal = 'B'
+    }
+    else if(scoreB){
+        scoreFinal = 'C'
+    }
+    else if(scoreC){
+        scoreFinal = 'D'
+    }
+    else if(scoreD){
+        scoreFinal = 'E'
+    }
+    else if(scoreF){
+        scoreFinal = 'F'
+    } else{
+        scoreFinal = "Nota inválida"
+    }
+
+console.log(scoreFinal)
+
+//Fazendo dentro uma uma function
+/*
+
+    function getScore(score) {
+let scoreA = score >= 90 && score <= 100
+let scoreB = score >= 80 && score <= 89
+let scoreC = score >= 70 && score <= 79
+let scoreD = score >= 60 && score <= 69
+let scoreF = score < 60 && score >= 0
+let scoreFinal;
+
+    if (scoreA){
+        scoreFinal = 'A'
+    } else if(scoreB){
+        scoreFinal = 'B'
+    }
+    else if(scoreB){
+        scoreFinal = 'C'
+    }
+    else if(scoreC){
+        scoreFinal = 'D'
+    }
+    else if(scoreD){
+        scoreFinal = 'E'
+    }
+    else if(scoreF){
+        scoreFinal = 'F'
+    } else{
+        scoreFinal = "Nota inválida"
+    }
+return scoreFinal
+}
+console.log(getScore(50))
+console.log(getScore(60))
+console.log(getScore(70))
+console.log(getScore(80))
+console.log(getScore(91))
+
+*/
