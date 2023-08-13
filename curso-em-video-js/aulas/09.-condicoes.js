14511bc9a669fc826ebde87b2772728ba8a25a54
Pedro <pedrogrego1@outlook.com>
2023-08-13 00:44:02 -0300

    Praticando JS

---
+++ curso-em-video-js/aulas/09.-condicoes.js
@@ -0,0 +1,14 @@
+var vel = 51
+
+if (vel > 0 && vel <= 50){
+    console.log('velocidade correta')
+} else{
+    console.log('Passou a velocidade, multado')
+}
+
+var pais = 'Brasil'
+if (pais != 'Brasil'){
+    console.log('Estrangeiro')
+} else{
+    console.log ('Brasileiro')
+}
