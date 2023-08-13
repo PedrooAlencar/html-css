f89b063e66119ec2a2ea22a42232522e30422823
Pedro <pedrogrego1@outlook.com>
2023-08-13 10:08:59 -0300

    Estudando Estrutura de repetição

---
+++ curso-em-video-js/aulas/13-repeticao.js
@@ -0,0 +1,15 @@
+//white - enquanto
+
+var c = 1
+
+while(c < 10000){
+    console.log(c)
+    c++
+}
+console.log(`Chegamos ao número ${c}`)
+
+function comerPizza(){
+    while(condicao){
+
+    }
+}
